bdb9b866190f4f02b96146e5219d34b31998beef
bryan <bryanboni74@gmail.com>
2015-12-14 15:49:32 +0100

    Gestion Hebergement
    Completion de la classe CompteHebergement

--- GestionHebergement/index.php
+++ GestionHebergement/index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-
-
 <html>
     <head>
         <meta charset="UTF-8">
         <title>  BED_TENNIS </title>
+        <link rel="stylesheet" type="text/css" href="Vue/main.css">
     </head>
    
     <body>
         <center>
+        <div id ="corps">
             <h1> Bienvenu sur le Site de BED Tennis </h1>
             <h2> veuillez vous identifier en cliquant ici :</h2>
-            <a href=""> Me Connecter </a>
+            <a href=""><div id = "bouton"><h3> Connexion </h3></div></a>
+        </div>
         </center>
     </body>
 </html>
